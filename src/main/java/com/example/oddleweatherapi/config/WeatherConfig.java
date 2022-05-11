8351282b09251514a6a488b4ba9972dc257b04f9
phnam3 <phnam3@cmcgloabl.vn>
2022-05-11 16:16:30 +0700

    Use liquibase to create sample data instead of WeatherConfig.

--- src/main/java/com/example/oddleweatherapi/config/WeatherConfig.java
+++ src/main/java/com/example/oddleweatherapi/config/WeatherConfig.java
@@ -13,22 +13,9 @@ import java.util.List;
 @Configuration
 public class WeatherConfig {
 
-    //TODO: implement with liquidbase to create table and insert sample data for city, weathertype,
-    //sample weather
     @Bean
     CommandLineRunner commandLineRunner(WeatherRepository repository){
         return args -> {
-            City c1 = new City(2);
-            WeatherType wt1 = new WeatherType(1);
-            Weather weather1 = new Weather("25C", "15C", "30C",
-                    "2022-01-01", wt1, c1);
-
-            City c2 = new City(4);
-            WeatherType wt2 = new WeatherType(2);
-            Weather weather2 = new Weather("255C", "155C", "305C",
-                    "2022-01-01", wt2, c2);
-
-            repository.saveAll(List.of(weather1, weather2));
         };
     }
 }
