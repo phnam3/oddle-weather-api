e4f54429f2d512103dadcdc70411cd365f200a65
phnam3 <phnam3@cmcglobal.vn>
2022-06-16 10:18:16 +0700

    refactor! (security): remove vulnerability

--- src/main/java/com/example/oddleweatherapi/controller/WeatherController.java
+++ src/main/java/com/example/oddleweatherapi/controller/WeatherController.java
@@ -6,6 +6,7 @@ import com.example.oddleweatherapi.model.Weather;
 import com.example.oddleweatherapi.service.WeatherService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/example/oddleweatherapi/model/City.java
+++ src/main/java/com/example/oddleweatherapi/model/City.java
@@ -3,6 +3,7 @@ package com.example.oddleweatherapi.model;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
